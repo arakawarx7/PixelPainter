6e74fbb9edf2c75c40260752cba9bb427310d20a
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-09-16 16:40:00 -1000

    Created the canvas and its cells
    cells toggle between black and white when clicked

---
+++ js/PixelPainter.js
@@ -0,0 +1,45 @@
+/*
+*  PixelPainter creates a canvas that can be painted on.
+* @param {Number} width  --> Width of the canvas, in number of cells
+* @param {Number} height --> Height of the canvas, in number of cells
+*/
+
+function pixelPainter(width, height) {
+  var module = {};
+  var ppDiv = document.getElementById('pixelPainter');
+  var ppCanvas = document.createElement('div');
+  var pixelSize = 8;
+
+  ppCanvas.style.width = width * pixelSize;
+  ppCanvas.style.height = height * pixelSize;
+  ppCanvas.style.position = "absolute";
+
+  module.changeColor = function(e) {
+    if(e.target.style.backgroundColor === "white") {
+      e.target.style.backgroundColor = "black";
+    } else {
+      e.target.style.backgroundColor = "white";
+    }
+  };
+
+  //do work here
+  for(var y = 0; y < height; y++) {
+    for(var x = 0; x < width; x++) {
+      var pixCell = document.createElement('div');
+      pixCell.style.width = pixelSize;
+      pixCell.style.height = pixelSize;
+      pixCell.style.border = "1px dotted black";
+      pixCell.style.position = "absolute";
+      pixCell.style.left = "" + (x * pixelSize);
+      pixCell.style.top = "" + (y * pixelSize);
+      pixCell.style.backgroundColor = "white";
+      pixCell.addEventListener('click', module.changeColor);
+      ppCanvas.appendChild(pixCell);
+    }
+  }
+
+  ppDiv.appendChild(ppCanvas);
+  return module;
+}
+
+var pp = pixelPainter(64, 64);
